dcd416109908d24ecc6a461d1f4d0c287e65678a
nithinmv4u <nithinmv4u@gmail.com>
2023-11-07 18:18:35 +0400

    backend setup

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Tasks
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views 
 class TaskCreate(generics.ListCreateAPIView):
-    queryset = Tasks.objects.all()
+    permission_classes = [IsAuthenticated]
+    # queryset = Tasks.objects.all()
     serializer_class = TaskSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        return Tasks.objects.filter(user = user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
 class TaskRetriveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Tasks.objects.all()
+    permission_classes = [IsAuthenticated]
+    # queryset = Tasks.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tasks.objects.filter(user = user)
+    
+    def perform_update(self, serializer):
+        return super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+        
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
 
 # Create your views here.
+class SignUpView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
